1d4bd298fb79c06eb511cb2dad8cfeafb1e5f65e
Oliver Wales <oli@opvia.bio>
2024-09-11 17:55:43 -0700

    Build, doh

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,40 +1,57 @@
-declare class ArvaBase {
+export declare class ArvaBase {
     api_key: string;
     base_url: string;
     customers: Customers;
     constructor(api_key: string, base_url: string);
 }
+export type CreateCustomerInput = {
+    agentId: string;
+    registeredName: string;
+    state?: string;
+};
+export type CreateCustomerResponse = {
+    id: string;
+};
+export type UpdateCustomerInput = {
+    id: string;
+    userInfoPatch: Record<string, unknown>;
+    websites: string[];
+    files: {
+        buffer: Buffer;
+        name: string;
+    }[];
+};
+export type UpdateCustomerResponse = {
+    success: boolean;
+    result: {
+        verdict: "ACCEPT" | "REJECT";
+        lowConfidence: boolean;
+        rfi: never;
+    } | {
+        verdict: "REQUEST INFORMATION";
+        lowConfidence: boolean;
+        rfi: string;
+    };
+};
+export type ReviewCustomerInput = {
+    id: string;
+    verdict: "ACCEPT" | "REJECT" | "REQUEST INFORMATION";
+    reason: string;
+    rfi?: string;
+};
 declare class Customers {
     private arva_instance;
     constructor(arva_instance: ArvaBase);
-    create({ agentId, registeredName, state, }: {
-        agentId: string;
-        registeredName: string;
-        state?: string;
-    }): Promise<{
-        id: string;
-    }>;
-    update({ id, userInfoPatch, websites, files, }: {
-        id: string;
-        userInfoPatch: Record<string, unknown>;
-        websites: string[];
-        files: {
-            buffer: Buffer;
-            name: string;
-        }[];
-    }): Promise<{
-        customerId: string;
-    }>;
-    review({ id, verdict, reason, rfi, }: {
-        id: string;
-        verdict: "ACCEPT" | "REJECT" | "REQUEST INFORMATION";
-        reason: string;
-        rfi?: string;
-    }): Promise<unknown>;
+    create({ agentId, registeredName, state }: CreateCustomerInput): Promise<CreateCustomerResponse>;
+    update({ id, userInfoPatch, websites, files }: UpdateCustomerInput): Promise<UpdateCustomerResponse>;
+    review({ id, verdict, reason, rfi }: ReviewCustomerInput): Promise<void>;
 }
 export declare class Arva extends ArvaBase {
     constructor(api_key: string);
 }
+/**
+ * This is just for testing against a local instance of the Arva API.
+ */
 export declare class ArvaLocal extends ArvaBase {
     constructor(api_key: string);
 }
--- dist/index.js
+++ dist/index.js
@@ -1,136 +1,83 @@
 "use strict";
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator["throw"](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ArvaLocal = exports.Arva = void 0;
+exports.ArvaLocal = exports.Arva = exports.ArvaBase = void 0;
 const axios_1 = __importDefault(require("axios"));
 const form_data_1 = __importDefault(require("form-data"));
 class ArvaBase {
-  constructor(api_key, base_url) {
-    this.api_key = api_key;
-    this.base_url = base_url;
-    this.customers = new Customers(this);
-  }
+    constructor(api_key, base_url) {
+        this.api_key = api_key;
+        this.base_url = base_url;
+        this.customers = new Customers(this);
+    }
 }
+exports.ArvaBase = ArvaBase;
 class Customers {
-  constructor(arva_instance) {
-    this.arva_instance = arva_instance;
-  }
-  create(_a) {
-    return __awaiter(
-      this,
-      arguments,
-      void 0,
-      function* ({ agentId, registeredName, state }) {
-        const response = yield axios_1.default.post(
-          this.arva_instance.base_url + "/customer/create",
-          {
-            agentId,
-            registeredName,
-            state,
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${this.arva_instance.api_key}`,
-            },
-          }
-        );
-        return response.data;
-      }
-    );
-  }
-  update(_a) {
-    return __awaiter(
-      this,
-      arguments,
-      void 0,
-      function* ({ id, userInfoPatch, websites, files }) {
-        const form = new form_data_1.default();
-        form.append("customerId", id);
-        form.append("userInfoPatch", JSON.stringify(userInfoPatch));
-        form.append("websites", JSON.stringify(websites));
-        for (const file of files) {
-          form.append("file", file.buffer, file.name);
-        }
-        const response = yield axios_1.default.post(
-          this.arva_instance.base_url + "/customer/update",
-          form,
-          {
-            headers: Object.assign(
-              { Authorization: `Bearer ${this.arva_instance.api_key}` },
-              form.getHeaders()
-            ),
-          }
-        );
-        return response.data;
-      }
-    );
-  }
-  review(_a) {
-    return __awaiter(
-      this,
-      arguments,
-      void 0,
-      function* ({ id, verdict, reason, rfi }) {
-        const response = yield axios_1.default.post(
-          this.arva_instance.base_url + "/customer/review",
-          { customerId: id, verdict, reason, rfi },
-          {
-            headers: {
-              Authorization: `Bearer ${this.arva_instance.api_key}`,
-            },
-          }
-        );
-        return response.data;
-      }
-    );
-  }
+    constructor(arva_instance) {
+        this.arva_instance = arva_instance;
+    }
+    create(_a) {
+        return __awaiter(this, arguments, void 0, function* ({ agentId, registeredName, state }) {
+            const response = yield axios_1.default.post(this.arva_instance.base_url + "/customer/create", {
+                agentId,
+                registeredName,
+                state,
+            }, {
+                headers: {
+                    Authorization: `Bearer ${this.arva_instance.api_key}`,
+                },
+            });
+            return response.data;
+        });
+    }
+    update(_a) {
+        return __awaiter(this, arguments, void 0, function* ({ id, userInfoPatch, websites, files }) {
+            const form = new form_data_1.default();
+            form.append("customerId", id);
+            form.append("userInfoPatch", JSON.stringify(userInfoPatch));
+            form.append("websites", JSON.stringify(websites));
+            for (const file of files) {
+                form.append("file", file.buffer, file.name);
+            }
+            const response = yield axios_1.default.post(this.arva_instance.base_url + "/customer/update", form, {
+                headers: Object.assign({ Authorization: `Bearer ${this.arva_instance.api_key}` }, form.getHeaders()),
+            });
+            return response.data;
+        });
+    }
+    review(_a) {
+        return __awaiter(this, arguments, void 0, function* ({ id, verdict, reason, rfi }) {
+            yield axios_1.default.post(this.arva_instance.base_url + "/customer/review", { customerId: id, verdict, reason, rfi }, {
+                headers: {
+                    Authorization: `Bearer ${this.arva_instance.api_key}`,
+                },
+            });
+        });
+    }
 }
 class Arva extends ArvaBase {
-  constructor(api_key) {
-    super(api_key, "http://platform.arva-ai.com/api/v0");
-  }
+    constructor(api_key) {
+        super(api_key, "http://platform.arva-ai.com/api/v0");
+    }
 }
 exports.Arva = Arva;
+/**
+ * This is just for testing against a local instance of the Arva API.
+ */
 class ArvaLocal extends ArvaBase {
-  constructor(api_key) {
-    super(api_key, "http://localhost:3000/api/v0");
-  }
+    constructor(api_key) {
+        super(api_key, "http://localhost:3000/api/v0");
+    }
 }
 exports.ArvaLocal = ArvaLocal;
